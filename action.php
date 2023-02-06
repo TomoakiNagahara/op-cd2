5119aef18e4c3cb7771686f18ecf2c85930eeaa8
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-02-07 01:55:30 +0900

    New: action.php

---
+++ action.php
@@ -0,0 +1,32 @@
+<?php
+/** op-cd2:/action.php
+ *
+ * @created    2023-01-02
+ * @moved      2023-02-05 from op-cd1
+ * @version    1.0
+ * @package    op-cd2
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+//	...
+error_reporting(E_ALL);
+ini_set('short_open_tag', 1);
+ini_set('display_errors', 1);
+ini_set('log_errors'    , 0);
+
+//	...
+chdir(__DIR__);
+
+//	...
+require('Error.php');
+require('Debug.php');
+require('Display.php');
+require('Request.php');
+require('CD2.class.php');
+
+//	...
+$exit = CD2::Auto() ? 0: 1;
+
+//	...
+exit($exit);
