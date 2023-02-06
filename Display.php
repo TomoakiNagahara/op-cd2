ae6fbe9e912374049f92febcd1b2e7ba93080586
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-02-07 01:58:39 +0900

    New: Display.php

---
+++ Display.php
@@ -0,0 +1,38 @@
+<?php
+/** op-cd2:/Display.php
+ *
+ * @created    2023-01-02
+ * @moved      2023-02-05 from op-cd1
+ * @version    1.0
+ * @package    op-cd2
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Display message
+ *
+ * @created    2023-01-02
+ */
+function Display(string $message)
+{
+	//	...
+	static $_display = null;
+	static $_debug   = null;
+
+	//	...
+	if( $_display === null ){
+		$_display = Request('display') ?? 1;
+		$_debug   = Request('debug')   ?? 0;
+	}
+
+	//	...
+	if( $_display ){
+		//	...
+		echo $message . "\n";
+
+		//	...
+		if( $_debug > 1 ){
+			DebugTrace(debug_backtrace());
+		}
+	}
+}
