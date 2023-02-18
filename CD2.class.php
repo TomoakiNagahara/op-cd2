e24345cca4708e14f19fb84039c3cb59e9831f33
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-02-18 13:36:26 +0900

    New: CD2.class.php

---
+++ CD2.class.php
@@ -0,0 +1,21 @@
+<?php
+/** op-cd2:/CD2.class.php
+ *
+ * @created    2023-02-05
+ * @version    1.0
+ * @package    op-cd2
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Code Delivery
+ *
+ * @created    2023-02-05
+ * @version    1.0
+ * @package    op-cd2
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+class CD2
+{
+}
