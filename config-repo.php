ce914a788d8e7f5125132d1f0da64db314bdf1b3
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-03-12 13:04:22 +0900

    Fix: config-repo.php | Fix a bit.

--- config-repo.php
+++ config-repo.php
@@ -12,11 +12,11 @@
 //	...
 $config = [];
 $config['workspace'] = '/www/workspace/';
-$config['upstream']  = 'repo:~/repo/op/skeleton/2022.git';
 $config['origin']    = '~/repo/op/skeleton/2022.git';
+$config['upstream']  = 'repo:~/repo/op/skeleton/2022.git';
 $config['github']    = 'TomoakiNagahara'; // GitHub account (user name)
 $config['branch']    = 'master'; // This is parent branch. Each submodules branch is .gitmodules.
-$config['directory'] = '2022';   // Git clone use this directory name
+$config['directory'] = '2022-repo';   // Git clone use this directory name
 $config['gitmodules']=[ // Which .gitmodules file.
 	'origin'   => 'local',
 	'upstream' => 'repo',
