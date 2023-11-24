9a06ea90446b1d5d808761e735e458a5fd7c78e4
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-11-25 08:01:45 +0900

    Fix: CD2 :: CD() | Now specify the branch name of .gitmodules.

--- CD2.class.php
+++ CD2.class.php
@@ -336,7 +336,7 @@ class CD2
 		Display(" * All inspection is complete.");
 	}
 
-	/** CD
+	/** Code Delivery
 	 *
 	 * @created    2023-02-07
 	 * @param      string      $version Execute PHP version
@@ -363,10 +363,12 @@ class CD2
 		//	Submodules
 		$configs = self::SubmoduleConfig();
 		foreach( $configs as $config ){
+            //  ...
+            $branch = $config['branch'];
 			//	...
 			self::ChangeDirectory($config['path']);
 			//	...
-			self::Shell("php{$version} cd.php remote={$remote} display={$display} debug={$debug}");
+			self::Shell("php{$version} cd.php remote={$remote} branch={$branch} display={$display} debug={$debug}");
 		}
 
 		//	Main
